dbc77a4a038f43b55264afd0126e4a5c029ea109
Levent Durdalı <levent07@live.com>
2022-08-18 18:42:58 +0300

    Update country.py

--- Locaiton/Counrty Info/country.py
+++ Locaiton/Counrty Info/country.py
@@ -1,6 +1,21 @@
+# If possible find a way to do this in javascript
+# Add javascript version to my github page for daily use projects
+#
 from countryinfo import CountryInfo
 
 #country_input = input("Enter country name: ")
 country_input = "Turkey"
 
 country = CountryInfo(country_input)
+
+print("Capital: " + country.capital())
+print(country.languages())
+print("Population: " + str(country.population()))
+print("Region: " + country.region())
+print(country.timezones())
+print(country.tld())
+print(country.calling_codes())
+print(country.borders())
+print(country.demonym())
+print("nativeName: " + country.native_name())
+print(country.translations())
