c26d3017aaddb79dd7521b3aa7c496cf5763ed6f
Levent Durdalı <levent07@live.com>
2022-08-18 18:54:23 +0300

    Update geo.py

--- Locaiton/GeoCode/geo.py
+++ Locaiton/GeoCode/geo.py
@@ -6,8 +6,18 @@ from geopy.geocoders import Nominatim
 
 geolocation = Nominatim(user_agent="omerwwazap_testing")
 
-place_name = input("Enter place name: ")
+place_name = "Batman"
+# place_name = input("Enter place name: ")
 
 location = geolocation.geocode(place_name)
 
 print(location)
+
+zipcode_input = 13202
+# zipcode_input=input("Enter zipcode: ")
+
+location = geolocation.geocode(zipcode_input)
+
+print("Zipcode: " + str(zipcode_input))
+print("Details: " + str(location))
+print("Country: " + str(location.raw))
